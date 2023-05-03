8431a3f9ae3f8ec47acd116a0a351c2bd638bc3f
MMN <marcos_marvila@hotmail.com>
2023-05-03 20:27:50 -0300

    Atividades concluidas

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+let nome = prompt("Digite seu nome")
+
+let idade = Number(prompt("Digite sua idade"))
+
+// A variavel sem definição é undefined, devemos atribuir as variaveis comrretas para que o o programa funcione corretamente.
+
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log("Olá", nome, "Você tem", idade,"anos")
+
+
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,5 @@
+let pet = confirm("Você tem animais de estimação em casa?")
+let ali = confirm("Concorda que alimentação natural é a melhor opção?")
+let cont = confirm("Gostaria de ver quais nutrologos estão disponiveis em sua região?")
+
+console.log("O cliente possui", "Animais de estimação", pet, "Tem interesse em alimentação natural", ali, "Tem interesse em nutrologos disponiveis", cont )
